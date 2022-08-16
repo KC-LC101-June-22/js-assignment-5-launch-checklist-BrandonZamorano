66dafde52be5c0ba952d0a009a8c4be22e65b47d
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-08-16 15:44:59 -0500

    fetch planet data

--- scriptHelper.js
+++ scriptHelper.js
@@ -14,6 +14,17 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 </ol>
                 <img src="">
    */
+	document.getElementById('missionTarget').innerHTML = `
+                <h2>Mission Destination</h2>
+                <ol>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
+                </ol>
+                <img src="${imageUrl}">
+	`;
 }
 
 function validateInput(testInput) {
@@ -98,13 +109,13 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(res => res.json());
 
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+	return planets[Math.floor(Math.random() * planets.length)]
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
