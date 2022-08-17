92eab439acaf0a6e423a7589f34cdc9fa8ea2f92
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-08-16 20:34:40 -0500

    more fixes

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ window.addEventListener("load", function() {
 	const cargoMassInput = document.querySelector("input[name=cargoMass]");
 	const faultyItemsEl = document.getElementById('faultyItems');
 
+	// make the faultyItems list hidden to pass the test
+	// the test fails because it tries to read 'visibility' from the style property
+	// but the styles are not inline they're ready from an external css file
+	faultyItemsEl.style.visibility = "hidden";
+
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
 
